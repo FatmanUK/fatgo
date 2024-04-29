876d26edb0328e84700fb2e528932abb8ca5513b
Adam Richardson <adam.richardson@cgi.com>
2024-04-29 16:22:52 +0100

    Correct error in test script

--- test.bash
+++ test.bash
@@ -1,16 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-
-cd serialisers
-go test -v
-cd ..
-
-
 for D in *; do
 	[ ! -d ${D} ] && continue
 	pushd ${D} >/dev/null
 	go test -v && true
 	popd >/dev/null
 done
-
