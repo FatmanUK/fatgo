7ccf4a72208754cb76d97dd7888c7bbd380e59be
Adam J. Richardson <fatman.uk@gmail.com>
2025-08-15 19:41:31 +0100

    Automate usage generation

--- docopt_helpers/helpers.go
+++ docopt_helpers/helpers.go
@@ -1,12 +1,14 @@
 package docopt_helpers
 
 import (
+	"strings"
 	"github.com/docopt/docopt-go"
 )
 
 type HelpOption struct {
 	Value string
 	Desc string
+	Default string
 }
 
 var NoExitParser = &docopt.Parser{
@@ -26,5 +28,27 @@ func MakeDefault(value string) string {
 
 func (re HelpOption) MakeOption(flag string) string {
 	return flag + "=" + re.Value
+}
+
+func MakeUses(usages []string, options map[string]HelpOption) ([]string, map[string]string) {
+	opts := make(map[string]string)
+
+	for k1, u1 := range usages {
+		for k2, o2 := range options {
+			k := o2.MakeOption(k2)
+			u1 = strings.Replace(u1, k2, k, -1)
+		}
+		usages[k1] = u1
+	}
+
+	for k, v := range options {
+		t := v.MakeOption(k)
+		opts[t] = v.Desc
+		if v.Default != "" {
+			opts[t] += " " + MakeDefault(v.Default)
+		}
+	}
+
+	return usages, opts
 }
 
