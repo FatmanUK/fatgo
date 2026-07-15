4978119ddb84f31644a907ea37e7bad9d9f3162e
Adam J. Richardson <fatman.uk@gmail.com>
2026-07-15 13:02:08 +0100

    Add unified IsFileExists function

--- utils/file.go
+++ utils/file.go
@@ -2,17 +2,36 @@ package utils
 
 import (
 	"os"
+	"fmt"
+	"path/filepath"
 )
 
-func IsFileExists(path string) (bool, error) {
-	info, err := os.Stat(path)
+// isCreate: create directory if missing
+func IsFileExists(path string, isCreate bool) (bool, error) {
+	dir := filepath.Dir(path)
+	info, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		if isCreate {
+			err2 := os.MkdirAll(dir, 0750)
+			if err2 != nil {
+				return false, err2
+			}
+		} else {
+			m := fmt.Errorf("No such directory: %s", dir)
+			return false, m
+		}
+	} else if err != nil {
+		return false, err
+	}
+	if !info.IsDir() {
+		return false, fmt.Errorf("Not a directory: %s", dir)
+	}
+	info, err = os.Stat(path)
 	if err == nil {
 		return !info.IsDir(), nil
 	}
 	if os.IsNotExist(err) {
-		// doesn't exist
 		return false, nil
 	}
-	// permissions issue
 	return false, err
 }
