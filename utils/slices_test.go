23c1779dcb4f64aa33e4c321734354b42d5a7ddf
Adam J. Richardson <fatman.uk@gmail.com>
2026-07-16 11:57:17 +0100

    Add Compress and Decompress functions

--- utils/slices_test.go
+++ utils/slices_test.go
@@ -9,10 +9,10 @@ import (
 const FMT_TST_VAR = `Expected: "%v" Got: "%v"`
 
 const TST_OK_SLICES = `Slices match.`
-const TST_NO_SLICES_OK = `Slices match, but that's wrong.`
-
 const TST_NO_SLICES = `Slices do not match.`
+
 const TST_OK_SLICES_NO = `Slices do not match, but that's ok.`
+const TST_NO_SLICES_OK = `Slices match, but that's wrong.`
 
 func TestRemoveEmptyStrings_Must_Succeed(t *testing.T) {
 	testStrings := []string{
@@ -37,19 +37,12 @@ func TestCompareSlices_Must_Succeed(t *testing.T) {
 	as := []string{"abc", "123", "xyz"}
 	ai := []int{1, -8, 43}
 
-	bs := as
-	bi := ai
-
-	fs := []string{}
-	fi := []int{1, 9, -4}
+	bs := []string{"abc", "123", "xyz"}
+	bi := []int{1, -8, 43}
 
 	passes := []bool{}
 	passes = append(passes, CompareSlices(as, bs))
 	passes = append(passes, CompareSlices(ai, bi))
-
-	fails := []bool{}
-	fails = append(fails, CompareSlices(as, fs))
-	fails = append(fails, CompareSlices(ai, fi))
 
 	for _, pass := range passes {
 		if pass {
@@ -58,15 +51,24 @@ func TestCompareSlices_Must_Succeed(t *testing.T) {
 			t.Errorf(TST_NO_SLICES)
 		}
 	}
-
-	for _, fail := range fails {
-		if !fail {
-			t.Logf(TST_NO_SLICES_OK)
-		} else {
-			t.Errorf(TST_OK_SLICES_NO)
-		}
-	}
 }
 
 func TestCompareSlices_Must_Fail(t *testing.T) {
+	as := []string{"abc", "123", "xyz"}
+	ai := []int{1, -8, 43}
+
+	fs := []string{}
+	fi := []int{1, 9, -4}
+
+	fails := []bool{}
+	fails = append(fails, CompareSlices(as, fs))
+	fails = append(fails, CompareSlices(ai, fi))
+
+	for _, fail := range fails {
+		if !fail {
+			t.Logf(TST_OK_SLICES_NO)
+		} else {
+			t.Errorf(TST_NO_SLICES_OK)
+		}
+	}
 }
--- utils/strings.go
+++ utils/strings.go
@@ -3,6 +3,10 @@ package utils
 import (
 	"fmt"
 	"strconv"
+	"bytes"
+	"compress/zlib"
+	"encoding/base64"
+	"io"
 )
 
 func StringFromAny(any any) string {
@@ -12,4 +16,39 @@ func StringFromAny(any any) string {
 func Uint8FromHexString(s string) (uint8, error) {
 	u, err := strconv.ParseUint(s, 16, 8)
 	return uint8(u), err
+}
+
+// Compress shortens a string using zlib and encodes it to Base64
+func Compress(src string) (string, error) {
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	_, err := w.Write([]byte(src))
+	if err != nil {
+		return "", err
+	}
+	err = w.Close()
+	if err != nil {
+		return "", err
+	}
+	// Use RawURLEncoding to remove padding and make it URL safe
+	return base64.RawURLEncoding.EncodeToString(b.Bytes()), nil
+}
+
+func Decompress(src string) (string, error) {
+	cBytes, err := base64.RawURLEncoding.DecodeString(src)
+	if err != nil {
+		return "", err
+	}
+	b := bytes.NewReader(cBytes)
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		return "", err
+	}
+	defer r.Close()
+	var out bytes.Buffer
+	_, err = io.Copy(&out, r)
+	if err != nil {
+		return "", err
+	}
+	return out.String(), nil
 }
