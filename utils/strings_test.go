510db387f48a7e73fa12fb0d7ed009f748919dbd
Adam J. Richardson <fatman.uk@gmail.com>
2026-07-16 11:57:17 +0100

    Add Compress and Decompress functions

--- utils/strings_test.go
+++ utils/strings_test.go
@@ -11,3 +11,19 @@ func TestUint8FromHexString_Must_Succeed(t *testing.T) {
 func TestUint8FromHexString_Must_Fail(t *testing.T) {
 //func Uint8FromHexString(s string) (uint8, error) {
 }
+
+func TestCompress_Must_Succeed(t *testing.T) {
+//func Compress(src string) (string, error) {
+}
+
+func TestCompress_Must_Fail(t *testing.T) {
+//func Compress(src string) (string, error) {
+}
+
+func TestDecompress_Must_Succeed(t *testing.T) {
+//func Decompress(src string) (string, error) {
+}
+
+func TestDecompress_Must_Fail(t *testing.T) {
+//func Decompress(src string) (string, error) {
+}
